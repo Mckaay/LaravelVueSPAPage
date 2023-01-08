b0e2b6818a993f962022f6ee17f5efaa334657ec
Michal <michal1nycz@gmail.com>
2023-01-08 13:02:43 +0100

    Basic dashboard table for electric vehicles

--- resources/js/components/Vehicles/Index.vue
+++ resources/js/components/Vehicles/Index.vue
@@ -1,3 +1,56 @@
 <template>
-    Vue working...
+    <div class="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
+        <div class="min-w-full align-middle">
+            <table class="min-w-full divide-y divide-gray-200 border">
+                <thead>
+                <tr>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Id</span>
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Company</span>
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Model</span>
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Price</span>
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Year</span>
+                    </th>
+                    <th class="px-6 py-3 bg-gray-50 text-left">
+                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Action</span>
+                    </th>
+                </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200 divide-solid">
+                <tr>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">1</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">Tesla</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">Model Y Long Range Dual Motor</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">0</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">2020</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900"></td>
+                </tr>
+                <tr>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">2</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">Tesla</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">Model Y Long Range Dual Motor</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">0</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">2020</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900"></td>
+                </tr>
+                <tr>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">3</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">Tesla</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">Model Y Long Range Dual Motor</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">0</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">2020</td>
+                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900"></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </template>
